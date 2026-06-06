9f8cbecfceafff2b5092e3a6692c8a4002ba1d21
arima <arima3992@gmail.com>
2026-06-06 22:20:14 +0800

    fix: register vue-konva in main.js for planogram canvas editor

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,12 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import VueKonva from 'vue-konva'
 import router from './router'
 import App from './App.vue'
 import './assets/main.css'
 
 const app = createApp(App)
 app.use(createPinia())
+app.use(VueKonva)
 app.use(router)
 app.mount('#app')
